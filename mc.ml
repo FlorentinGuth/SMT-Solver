ad28ee2535c6c8e1e710e5ae4ecfe083ba860e5c
Kegnarok <kegnarok.rm@gmail.com>
2017-05-24 17:18:27 +0200

    Added simple model checker, who does not permit backtracking yet

--- mc.ml
+++ mc.ml
@@ -1,3 +1,6 @@
+module UF = Persistent_union_find
+
+
 type var = int
 type rel = Eq | Neq
 type atom = rel * var * var  (** A relation between two variables *)
@@ -9,5 +12,21 @@ type model = {
 }
 
 
+(** We check a model the following way:
+    - first, we use a union find structure in which every variable is alone,
+    - then, we consider all equalities and merge the adequate classes,
+    - last, if there is an inequality between two variables of the same class,
+      then the model is not satisfiable, otherwise it is *)
+let rec check_aux t f neqs =
+  match f with
+  | [] ->
+    List.for_all (fun (i, j) -> UF.find t i <> UF.find t j) neqs
+
+  | (Eq, i, j) :: tl ->
+    check_aux (UF.union t i j) tl neqs
+
+  | (Neq, i, j) :: tl ->
+    check_aux t tl ((i, j) :: neqs)
+
 let check model =
-  failwith "Not implemented"
+  check_aux (UF.create model.nb_var) model.f []
