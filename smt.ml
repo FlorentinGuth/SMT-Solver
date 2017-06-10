1f8eb10c9ce916d96977ecb91adaf2c06c96fd48
Kegnarok <kegnarok.rm@gmail.com>
2017-06-10 23:54:56 +0200

    Added big big tests and removed prints

--- smt.ml
+++ smt.ml
@@ -195,30 +195,33 @@ let neg_atom (a : atom) =
 
 
 let satisfiable cnf =
-  print_stdout "CNF: %a\n" print_cnf cnf;
+  (* print_stdout "CNF: %a\n" print_cnf cnf; *)
   let (sat_cnf, conv) = cnf_to_sat cnf in
   let check pseudo_model =
-    print_stdout "SAT solver found model %a\n" SAT.print_pseudo_model pseudo_model;
+    (* print_stdout "SAT solver found model %a\n" SAT.print_pseudo_model pseudo_model; *)
     (* We keep the old cnf and conv since we did not introduce any new variables *)
     let m_mc = model_to_mc pseudo_model conv cnf in
-    print_stdout "Calling Model Checker on %a\n" IMC.print_model m_mc;
+    (* print_stdout "Calling Model Checker on %a\n" IMC.print_model m_mc; *)
     match IMC.check m_mc with
     | None ->
-      print_stdout "Model Checker validated the model\n";
+      (* print_stdout "Model Checker validated the model\n"; *)
       []
 
     | Some l ->
-      print_stdout "Model Checker invalidated the model, adding negation\n";
+      (* print_stdout "Model Checker invalidated the model, adding negation\n"; *)
       let neg_expl = List.map neg_atom l in
       let sat_clause = match clause_to_sat conv cnf.var_of_app neg_expl with
         | CTrue | CFalse -> assert false
         | Clause cl -> cl in
-      print_stdout "Adding SAT clause %a\n" SAT.print_clause sat_clause;
+      (* print_stdout "Adding SAT clause %a\n" SAT.print_clause sat_clause; *)
       sat_clause
   in
-  print_stdout "Calling SAT solver on %a\n" SAT.print_cnf sat_cnf;
+  (* print_stdout "Calling SAT solver on %a\n" SAT.print_cnf sat_cnf; *)
   match SAT.solve sat_cnf check with
-  | None -> print_stdout "Unsatisfiable SAT formula\n"; false
-  | Some m -> print_stdout "SAT found model %a\n" SAT.print_model m;
+  | None ->
+    (* print_stdout "Unsatisfiable SAT formula\n"; *)
+    false
+  | Some m ->
+    (* print_stdout "SAT found model %a\n" SAT.print_model m; *)
     assert (SAT.test_model sat_cnf m);
     true
